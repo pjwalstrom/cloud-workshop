cb15a33bbe1d57912266cc4d755d4fae10eadc93
Stig Kleppe-Jørgensen <stig.kleppe-jorgensen@finn.no>
2015-05-18 15:58:15 +0200

    Install needed docker images
    Also add support for only installing docker-compose/-machine when
    not already present. This is for the case when one runs provision
    manually after vagrant up.

--- install.sh
+++ install.sh
@@ -7,17 +7,35 @@ echo ""
 apt-get update
 apt-get install --yes docker.io
 
+echo ""
+echo "Retrieving docker images"
+printf "  jeanblanchard/busybox-java:8..."
+docker pull jeanblanchard/busybox-java:8
 
-echo
-echo "Installing docker-compose..."
-curl -Ls https://github.com/docker/compose/releases/download/1.2.0/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+
+echo ""
+
+if [ -x docker-compose ]; then
+  echo "Already installed docker-compose..."
+else
+  echo "Installing docker-compose..."
+  curl -Ls https://github.com/docker/compose/releases/download/1.2.0/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+fi
+
 docker-compose --version
 
-echo
-echo "Installing docker-machine..."
-curl -Ls https://github.com/docker/machine/releases/download/v0.2.0/docker-machine_linux-amd64 > /usr/local/bin/docker-machine
-chmod +x /usr/local/bin/docker-machine
+
+echo ""
+
+if [ -x docker-machine ]; then
+  echo "Already installed docker-machine..."
+else
+  echo "Installing docker-machine..."
+  curl -Ls https://github.com/docker/machine/releases/download/v0.2.0/docker-machine_linux-amd64 > /usr/local/bin/docker-machine
+  chmod +x /usr/local/bin/docker-machine
+fi
+
 docker-machine --version
 
 
