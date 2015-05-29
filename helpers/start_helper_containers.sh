51dd9dd2ada75c1c4c920489077271fc131e41c2
Stig Kleppe-Jørgensen <stig.kleppe-jorgensen@finn.no>
2015-05-29 11:06:09 +0200

    Script that starts helper containers for discovery

---
+++ helpers/start_helper_containers.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo ""
+echo "Starting helper containers..."
+
+docker run --name consul -d -h vagrant -v /data \
+    -p 192.168.12.34:8300:8300 \
+    -p 192.168.12.34:8301:8301 \
+    -p 192.168.12.34:8301:8301/udp \
+    -p 192.168.12.34:8302:8302 \
+    -p 192.168.12.34:8302:8302/udp \
+    -p 192.168.12.34:8400:8400 \
+    -p 192.168.12.34:8500:8500 \
+    -p 172.17.42.1:53:53/udp \
+    progrium/consul -server -advertise 192.168.12.34 -bootstrap -ui-dir /ui
+
+docker run --name registrator -d -h vagrant \
+    -v /var/run/docker.sock:/tmp/docker.sock \
+    stigkj/registrator:v5.1 -ip 192.168.12.34 consul://192.168.12.34:8500
+
+# Must remove symbolic link created by the container from an eventual earlier run
+rm -f /vagrant/helpers/haproxy.tmpl
+docker run --name=haproxy -d \
+    -v /vagrant/helpers:/consul-template/template.d \
+    -e CONSUL_LOGLEVEL=debug \
+    -e CONSUL_CONNECT=192.168.12.34:8500 \
+    asteris/haproxy-consul
