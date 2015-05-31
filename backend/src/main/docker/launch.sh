744eae832e6d1818e4b8997961c7afe260f9fc30
Stig Kleppe-Jørgensen <stig.kleppe-jorgensen@finn.no>
2015-06-01 01:54:02 +0200

    If consul is not available do not use envconsul
    This means the app is started directly, not through envconsul.

--- backend/src/main/docker/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-envconsul \
-  -consul 192.168.12.34:8500 \
-  -prefix registrator/unique_port \
-  java \
-    -Djava.security.egd=file:/dev/./urandom \
-    -jar /tmp/app.war
---
+++ helpers/launch.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+CONSUL=192.168.12.34
+APP="java \
+      -Djava.security.egd=file:/dev/./urandom \
+      -jar /tmp/app.war"
+
+/usr/bin/curl \
+  --silent \
+  --output /dev/null \
+  $CONSUL:8500
+
+rc=$?
+
+if [ $rc != 0 ]; then
+  echo "WARNING! Consul not available, running locally."
+  $APP
+else
+  /usr/bin/envconsul \
+    -consul ${CONSUL}:8500  \
+    -prefix registrator/unique_port \
+    -upcase \
+    $APP
+fi
