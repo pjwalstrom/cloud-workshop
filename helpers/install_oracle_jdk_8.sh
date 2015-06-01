bdc0142c6546a77511857ed86bada5958115b344
Stig Kleppe-Jørgensen <stig.kleppe-jorgensen@finn.no>
2015-06-01 23:51:33 +0200

    Need some more stuff for installing jdk
    - Need to install the add-apt-repository tool
    - Need to make sure the oracle license are agreed to automatically
    - Say yes to all questions
    - Be almost quiet when installing, that is, no progress log

--- helpers/install_oracle_jdk_8.sh
+++ helpers/install_oracle_jdk_8.sh
@@ -9,9 +9,11 @@ if hash java 2> /dev/null; then
 else
   echo "Installing oracle jdk 8..."
   echo ""
+  apt-get install --quiet --yes software-properties-common
   add-apt-repository ppa:webupd8team/java
   apt-get update
-  apt-get install oracle-java8-installer
+  echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+  apt-get install --quiet --yes oracle-java8-installer
 fi
 
 java -version
